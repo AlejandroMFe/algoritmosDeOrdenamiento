f67d642050d0813139e13001f094af7a94cc6543
Alejandro <alejandro.mfe@gmail.com>
2021-09-20 21:01:51 -0300

    Add project files.

---
+++ MyExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace algoritmosDeOrdenamiento
+{
+    public static class MyExtensions
+    {
+        /// <summary>
+        /// Carga el arreglo con números del 1 al maxValue.
+        /// </summary>
+        /// <param name="arreglo"></param>
+        /// <returns></returns>
+        public static int[] Cargar(this int[] arreglo, int maxValue)
+        {
+            var rnd = new Random();
+
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                var nro = rnd.Next(1, maxValue);
+                arreglo[i] = nro;
+            }
+
+            return arreglo;
+        }
+
+        public static void Imprimir(this int[] arreglo)
+        {
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                Console.WriteLine($"#-{i} valor: {arreglo[i]}");
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Ordena el arreglo de menor a mayor, a través del algoritmo de Burbuja.
+        /// </summary>
+        /// <param name="arreglo"></param>
+        /// <returns></returns>
+        public static int[] OrdernarPorBurbuja(this int[] arreglo)
+        {
+            // Tamaño del arreglo.
+            var dimension = arreglo.Length;
+
+            // Recorre desde el inicio del arreglo
+            for (int i = 0; i < dimension; i++)
+            {
+                // Recorre desde la posición siguiente: i + 1
+                for (int x = i +1 ; x < dimension; x++)
+                {
+                    /**
+                     arreglo[i] tendría que ser el menor valor
+                     arreglo[x] es la posición sigueinte
+
+                     Si arreglo[i] es Mayor que arreglo[x]?
+                     Tengo que hacer el enrroque, para ordenar el arreglo.
+                    */
+                    if (arreglo[i] > arreglo[x])
+                    {
+                        // Enrroque
+                        var aux = arreglo[i];     // Guardo el mayor valor
+                        arreglo[i] = arreglo[x];  // Asigno el menor valor a la posición anterior
+                        arreglo[x] = aux;         // Asigno el mayor valor a la posición siguiente
+                    }
+                }
+            }
+
+            return arreglo;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace algoritmosDeOrdenamiento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var arreglo = new int[10];
+            
+            arreglo.Cargar(100);
+
+            Console.WriteLine("Sin Ordenar");
+            arreglo.Imprimir();
+
+            Console.WriteLine("Ordenado por Burbuja");
+            
+            var watch = Stopwatch.StartNew();
+            arreglo.OrdernarPorBurbuja();
+            watch.Stop();
+            arreglo.Imprimir();
+            Console.WriteLine($"Cuanto tardo en ordenar? {watch.Elapsed}");           
+        }
+
+    }
+}
